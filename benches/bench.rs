0f34a84340764f1076d0cee9f8fe137402449bc2
Michael Rosenberg <michael@mrosenberg.pub>
2022-05-03 15:50:07 -0400

    Included AAD in benchmarks

--- benches/bench.rs
+++ benches/bench.rs
@@ -9,35 +9,47 @@ use aead::{generic_array::typenum::Unsigned, AeadCore, AeadInPlace, Key, NewAead
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
 use rand_core::RngCore;
 
-const KB: usize = 1024;
-const MB: usize = 1024 * KB;
+#[allow(non_upper_case_globals)]
+const KiB: usize = 1024;
+#[allow(non_upper_case_globals)]
+const MiB: usize = 1024 * KiB;
+
+//
+// We bench encryption and decryption of the given AEAD with (aad, msg) sizes of:
+// (1B, 1B), (32B, 32B), (128B, 128B), (1KiB, 1KiB), (8KiB, 8KiB), (64KiB, 64KiB), (1MiB, 1MiB)
+//
 
 fn bench_aead<A: NewAead + AeadInPlace>(c: &mut Criterion, name: &str) {
-    let mut buffer = vec![0u8; MB];
+    let mut buffer = vec![0u8; MiB];
+    let mut associated_data = vec![0u8; MiB];
     rand::thread_rng().fill_bytes(&mut buffer[..]);
+    rand::thread_rng().fill_bytes(&mut associated_data[..]);
 
     let key = Key::<A>::clone_from_slice(&buffer[0..<A as NewAead>::KeySize::USIZE]);
     let nonce = Nonce::<A>::clone_from_slice(&buffer[0..<A as AeadCore>::NonceSize::USIZE]);
-    let associated_data = b"";
     let aead = <A as NewAead>::new(&key);
 
     let mut group = c.benchmark_group(name);
-    for size in [1, 32, 128, KB, 8 * KB, 64 * KB, MB].iter() {
+    for size in [1, 32, 128, KiB, 8 * KiB, 64 * KiB, MiB].iter() {
         group.throughput(Throughput::Bytes(*size as u64));
 
         group.bench_with_input(
-            BenchmarkId::from_parameter(format!("encrypt {: >8}", size)),
+            BenchmarkId::from_parameter(format!("encrypt [msg=aad={}B]", size)),
             size,
             |b, &size| {
                 b.iter(|| {
-                    aead.encrypt_in_place_detached(&nonce, associated_data, &mut buffer[0..size])
-                        .expect("encryption failure!")
+                    aead.encrypt_in_place_detached(
+                        &nonce,
+                        &associated_data[0..size],
+                        &mut buffer[0..size],
+                    )
+                    .expect("encryption failure!")
                 });
             },
         );
 
         let tag = aead
-            .encrypt_in_place_detached(&nonce, associated_data, &mut buffer[0..*size])
+            .encrypt_in_place_detached(&nonce, &associated_data[0..*size], &mut buffer[0..*size])
             .expect("encryption failure!");
         group.bench_with_input(
             BenchmarkId::from_parameter(format!("decrypt {: >8}", size)),
@@ -45,8 +57,13 @@ fn bench_aead<A: NewAead + AeadInPlace>(c: &mut Criterion, name: &str) {
             |b, &size| {
                 b.iter(|| {
                     let mut buf_copy = buffer[0..size].to_vec();
-                    aead.decrypt_in_place_detached(&nonce, associated_data, &mut buf_copy, &tag)
-                        .expect("decryption failure!");
+                    aead.decrypt_in_place_detached(
+                        &nonce,
+                        &associated_data[0..size],
+                        &mut buf_copy,
+                        &tag,
+                    )
+                    .expect("decryption failure!");
                 })
             },
         );
